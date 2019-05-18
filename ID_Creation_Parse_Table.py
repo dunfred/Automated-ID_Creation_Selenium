aa0f125c80bde05f4833aafd8a52abae7a5b9bdc
dunfred <43072903+dunfred@users.noreply.github.com>
2019-05-18 15:33:24 +0000

    Add files via upload

---
+++ ID_Creation_Parse_Table.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+from bs4 import BeautifulSoup as sp
+from selenium import webdriver
+import pandas as pd
+import numpy as np
+import time
+import re
+
+#Step ONE(1) +Downloading the reports file
+olm_id = '******'
+password = '******'
+
+driver = webdriver.Chrome("/home/klenam/Downloads/WebDrivers/chromedriver_linux64/chromedriver")
+driver.get("https://airtel.service-now.com")
+driver.maximize_window()
+driver.implicitly_wait(30)
+
+#Locates username login field ,passes in username and submits
+UsrName_field = driver.find_element_by_id("username")
+UsrName_field.send_keys(olm_id)
+UsrName_field.send_keys(Keys.RETURN)
+
+driver.implicitly_wait(60)
+b = driver.find_element_by_xpath('//*[@id="bySelection"]/div[2]/div/span')
+b.click()
+
+#Fill in Login Form
+driver.implicitly_wait(60)
+el_field = driver.find_element_by_id("userNameInput")
+el_field.send_keys(olm_id)
+text_field = driver.find_element_by_id("passwordInput")
+text_field.send_keys(password)
+text_field.send_keys(Keys.RETURN)
+time.sleep(10)
+
+#click Reports Tab
+reports_tab = driver.find_element_by_css_selector("#gsft_nav > div > magellan-favorites-list > ul > li.sn-widget.ng-scope.selected.ui-sortable-handle > div > div:nth-child(1) > a")
+reports_tab.click()
+
+#switch to iframe
+driver.implicitly_wait(60)
+driver.switch_to.frame("gsft_main") #//*[@id="gsft_main"]
+time.sleep(3)
+SMP_UID = driver.find_element_by_xpath('//*[@id="report-list"]/tbody/tr[2]/td[4]/a')
+SMP_UID.click()
+
+#parsing iframe with BeautifulSoup
+iframe_src = driver.page_source
+soup= sp(iframe_src, "lxml")
+inc_tab = soup.find_all("table", {"class":"data_list_table list_table table table-hover " })
+
+driver.implicitly_wait(30)
+time.sleep(3)
+tableRow = soup.find('tr', {'id':re.compile(r"hdr_\w{32}") })['id']
+
+ID = str(tableRow[4:])
+action_chains = ActionChains(driver) 
+
+time.sleep(10)
+
+#locate NumberTab column by randomly generated IDs
+NumberTab = driver.find_element_by_xpath('//*[@id="hdr_' + str(ID) +'"]/th[1]/span').click()
+action_chains.context_click(NumberTab)
+action_chains.perform() 
+driver.implicitly_wait(30)
+
+#find export in popup menu and click
+export = driver.find_element_by_xpath('//*[@id="context_list_header' + str(ID) + '"]/div[10]').click()
+driver.implicitly_wait(30)
+
+#find Excel in export popup menu and click
+exportToExcel = driver.find_element_by_xpath("//div[contains(text(), 'Excel (.xlsx)')]").click()
+time.sleep(10)
+
+#click on download
+DownloadExcel = driver.find_element_by_xpath('//*[@id="download_button"]').click()
+
+#Step TWO(2) +Cleaning downloaded file
+time.sleep(10)
+df = pd.read_excel('/home/klenam/Downloads/sc_req_item.xlsx')
+df['Requested for'] = df['Requested for'].str.split('(', n=1, expand=True)
+df1 = df.drop(['Business Justification', 'State'], axis=1) 
+new = df1["Requested for"].str.split(" ", n = 1, expand = True)
+df1["First Name"]= new[0]
+df1["Last Name"]= new[1] 
+df1['Last Name'] = df1['Last Name'].str.title()
+
+df2 = df1.sort_values(by=['Req Subtype'])
+df3 = df2.reset_index(drop=True)
+df4 = df3.loc[df3['Req For'] == 'ID Creation']
+df5 = df4.loc[df3['Stage'] == 'Implementation']
+df_olm = df5.loc[df4['UID Type']== 'OLMID']
+df_oth = df5.loc[df4['UID Type']== 'OTHER']
+
+df_olm_isibm = df_olm[df_olm['Email'].str.contains("ibm")] 
+df_olm_iscus = df_olm[~df_olm['Email'].str.contains("ibm")]
+df_other_isibm = df_oth[df_oth['Email'].str.contains("ibm")] 
+df_other_iscus = df_oth[~df_oth['Email'].str.contains("ibm")]
+
+
+
+try:
+    df_olm_iscus['Taging'] = df_olm_iscus.apply(lambda x:'744/C/%s//%s_%s_%s_%s_I_%s' % (x['User ID'],x['First Name'],x['Last Name'],x['User ID'],x['Req Subtype'],x['Number']),axis=1)
+except ValueError:
+    pass
+try:
+    df_olm_isibm['Taging'] = df_olm_isibm.apply(lambda x:'744/I/%s//%s_%s_%s_%s_I_%s' % (x['User ID'],x['First Name'],x['Last Name'],x['User ID'],x['Req Subtype'],x['Number']),axis=1)
+except ValueError:
+    pass
+try:
+    df_other_iscus['Taging'] = df_other_iscus.apply(lambda x:'744/C/%s//%s_%s_%s_%s_I_%s' % (x['User ID'],x['First Name'],x['Last Name'],x['User ID'],x['Req Subtype'],x['Number']),axis=1)
+except ValueError:
+    pass
+try:
+    df_other_isibm['Taging'] = df_other_isibm.apply(lambda x:'744/F/%s//%s_%s_%s_%s_I_%s' % (x['User ID'],x['First Name'],x['Last Name'],x['User ID'],x['Req Subtype'],x['Number']),axis=1)
+except ValueError:
+    pass
+
+frames = [df_olm_iscus, df_olm_isibm, df_other_iscus, df_other_isibm]
+df_orignal = pd.concat(frames, sort=False)
+df_orignal.index = np.arange(1,len(df_orignal)+1)
+
+#Save and export to ID_creation_Details.xlsx file
+t = df_orignal.to_excel('/home/klenam/Documents/Python Second Project/ID_creation_Details.xlsx')
+
+data = pd.read_excel('/home/klenam/Documents/Python Second Project/ID_creation_Details.xlsx')
+data
+print("done")
+
+
+
+
---
+++ prototype_final_phase.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+from bs4 import BeautifulSoup as sp
+from selenium import webdriver
+from datetime import timedelta
+from datetime import datetime
+import pandas as pd
+import time
+import re
+
+rota_path = r'windows_rota_updated.xlsx'
+
+daytime = datetime.now()
+cur_t = daytime.strftime('%H')
+cur_t = int(cur_t)
+#cur_t = 4
+n_1 = daytime - timedelta(days=1)
+
+cur_d = int(daytime.strftime('%d'))
+
+pre_d = int(n_1.strftime('%d'))
+#pre_d = 23
+df = pd.read_excel(rota_path, index_col=cur_d)
+
+m = list(range(8,11))
+m_g = list(range(11,15))
+m_g_e = list(range(15,17))
+g_e = list(range(17,19))
+e = list(range(19,22))
+n = [22,23]
+n1 = list(range(0,8))
+
+if cur_t in m:
+    user = df.loc['M']
+elif cur_t in m_g:
+    user = df.loc[['M','G']]
+elif cur_t in m_g_e:
+    user = df.loc[['M','G','E']]
+elif cur_t in g_e:
+    user = df.loc[['G','E']]
+elif cur_t in e:
+    user = df.loc['E'] 
+elif cur_t in n:
+    user = df.loc['N']
+elif cur_t in n1:
+    df = pd.read_excel(rota_path, index_col=pre_d)
+    user = df.loc['N']
+else:
+    print('Item not found')
+user_name = user.Name.tolist()
+print(user_name)
+
+#Portal section starts here
+olm_id = 'A1yizb18'
+password = 'noida@35'
+
+driver = webdriver.Chrome("/home/klenam/Downloads/WebDrivers/chromedriver_linux64/chromedriver")
+driver.get("https://airtel.service-now.com/nav_to.do?uri=%2Fincident_list.do%3Fsysparm_fixed_query%3Dactive%3Dtrue%5Eassignment_groupDYNAMICd6435e965f510100a9ad2572f2b47744%26%3D%26sysparm_query%3DstateNOT%2520IN6,7,8%5EEQ%26sysparm_userpref_module%3Db55fbec4c0a800090088e83d7ff500de%26sysparm_clear_stack%3Dtrue")
+driver.maximize_window()
+driver.implicitly_wait(10)
+
+email_field = driver.find_element_by_id("username")
+email_field.send_keys("A1yizb18")
+email_field.send_keys(Keys.RETURN)
+
+driver.implicitly_wait(60)
+b = driver.find_element_by_xpath('//*[@id="bySelection"]/div[2]/div/span')
+b.click()
+
+driver.implicitly_wait(60)
+el_field = driver.find_element_by_id("userNameInput")
+el_field.send_keys(olm_id)
+text_field = driver.find_element_by_id("passwordInput")
+text_field.send_keys(password)
+text_field.send_keys(Keys.RETURN)
+
+driver.implicitly_wait(60)
+#find and select Incidents tab and click on it
+incidents = driver.find_element_by_xpath('/html/body/div/div/div/nav/div/div[3]/div/div/magellan-favorites-list/div[2]/div[3]/div/a').click()
+
+driver.implicitly_wait(60)
+#Switch to iframe on webpage
+driver.switch_to.frame("gsft_main")
+time.sleep(10)
+
+#driver.implicitly_wait(60)
+iframe = driver.page_source
+src = sp(iframe, 'lxml')
+
+#list of rows numbers we are selecting
+ListOfRows = []
+
+#parse iframe and get the length of the number table rows
+reg_id = re.compile(r"^row_incident.+$")
+soup = src.find_all('tr', {"id": reg_id})
+
+for tr in soup:
+    x = int(soup.index(tr)) + 1
+    souptr = sp(tr.encode("utf-8"), 'lxml')
+    name = re.compile(r'^\w+ \w+\(\w+\)$')
+    text = souptr.find_all('a', {"class":"linked"})
+    
+    cnfrm = False
+    for links in text:
+        if name.search(links.text) != None:
+            cnfrm = True            
+        else:
+            continue
+    if cnfrm != True:
+        ListOfRows.append(int(x))
+    else:
+        continue
+
+#Funtion here takes a row number(i) and username
+def AssignUserName(i, username):
+    #select Assigned to column
+    table_row  = driver.find_element_by_xpath('//table[@id="incident_table"]/tbody/tr[{0}]/td[7]'.format(str(i)))
+    table_row.click()
+    time.sleep(2)
+    
+    #double click on Assigned to coulmn
+    actionChains = ActionChains(driver)
+    actionChains.double_click(table_row).perform()  
+    driver.implicitly_wait(10)
+    
+    #open popup menu and send text
+    popup = driver.find_element_by_css_selector("#sys_display\.LIST_EDIT_incident\.assigned_to").send_keys(username)
+    time.sleep(3)
+
+    #click green tick to send
+    #cmmd = driver.find_element_by_css_selector("#cell_edit_ok")
+    
+    #close the popup menu
+    cmmd = driver.find_element_by_css_selector("#cell_edit_cancel").click()  
+
+#pass in username for each table row
+
+cnt = 0
+for i in range(len(ListOfRows)):
+    try:
+        if cnt > int(len(user_name) -  1):
+            cnt = 0
+            AssignUserName(ListOfRows[i], user_name[cnt])
+        else:
+            AssignUserName(ListOfRows[i], user_name[cnt])
+        cnt += 1
+    except NoSuchElementException as problem:
+        print(problem,'\nOn row',str(ListOfRows[i]), "\n")
+        continue
+print("\nSuccessful\n")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
